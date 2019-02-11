96b8afea80121c711de944973bfffdc298db006b
Artem Bernatskyi <artem.bernatskyy@gmail.com>
2019-02-11 10:58:39 +0200

    building FE locally

---
+++ client/build/precache-manifest.251a797bcee69d5fda63405248f95ca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "923f9ce190bb71352412",
+    "url": "/static/js/main.923f9ce1.chunk.js"
+  },
+  {
+    "revision": "c9d644d38b1f959cb77c",
+    "url": "/static/js/2.c9d644d3.chunk.js"
+  },
+  {
+    "revision": "923f9ce190bb71352412",
+    "url": "/static/css/main.f77d932c.chunk.css"
+  },
+  {
+    "revision": "c9d644d38b1f959cb77c",
+    "url": "/static/css/2.d1d5c664.chunk.css"
+  },
+  {
+    "revision": "e59f68402ddcc2d98dbbef755dcf67ae",
+    "url": "/index.html"
+  }
+];
